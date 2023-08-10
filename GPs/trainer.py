27c892ae73ecf7d70617b618f90bbd2a9ebc97ae
Adeeb Abbas <adeebabbas@gmail.com>
2023-08-10 10:11:40 -0400

    chores

--- GPs/trainer.py
+++ GPs/trainer.py
@@ -4,5 +4,36 @@ import gpytorch
 from matplotlib import pyplot as plt
 from scipy.io import loadmat
 from math import floor
+from model import ExactGPModel
+from torch.utils.data import TensorDataset, DataLoader
 
 data = torch.Tensor(loadmat('3droad.mat')['data'])
+import numpy as np
+
+N = data.shape[0]
+# make train/val/test
+n_train = int(0.8 * N)
+train_x, train_y = data[:n_train, :-1], data[:n_train, -1]
+test_x, test_y = data[n_train:, :-1], data[n_train:, -1]
+
+# normalize features
+mean = train_x.mean(dim=-2, keepdim=True)
+std = train_x.std(dim=-2, keepdim=True) + 1e-6 # prevent dividing by 0
+train_x = (train_x - mean) / std
+test_x = (test_x - mean) / std
+
+# normalize labels
+mean, std = train_y.mean(),train_y.std()
+train_y = (train_y - mean) / std
+test_y = (test_y - mean) / std
+
+# make continguous
+train_x, train_y = train_x.contiguous(), train_y.contiguous()
+test_x, test_y = test_x.contiguous(), test_y.contiguous()
+
+output_device = torch.device('cuda:0')
+
+train_x, train_y = train_x.to(output_device), train_y.to(output_device)
+test_x, test_y = test_x.to(output_device), test_y.to(output_device)
+
+model.train()
